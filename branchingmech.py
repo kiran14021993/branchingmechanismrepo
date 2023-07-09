100568a6cbd5bfefd4f4398decb6521c1ce4e9d6
kiransurya90811@gmail.com <kiransurya90811@gmail.com>
2023-07-09 11:23:51 +0530

    This is def fun code in sample branch

--- branchingmech.py
+++ branchingmech.py
@@ -1,1 +1,6 @@
-print"This is branchmech file created in branchingmechanism repository"
+print("This is branchmech file created in branchingmechanism repository")
+def add(a,b):
+    c=a+b
+    return c
+addition=add(10,20)
+print(addition)
